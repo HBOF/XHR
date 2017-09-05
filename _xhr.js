0258eb013bb6796be2cdf355df721464253d1737
Спасибо <ty662@naver.com>
2017-09-05 15:09:54 +0900

    Update _xhr.js

--- _xhr.js
+++ _xhr.js
@@ -2,26 +2,31 @@ window.onload = function(){
 	var bt1 = document.getElementById('_bt1');
 
 	bt1.addEventListener('click', function() {
-		var xhr
+		var xhr;
 		if(window.XMLHttpRequest){
 			xhr = new XMLHttpRequest();
 		} else if (window.ActiveXObject){
 			xhr = new ActiveXObject("Microsoft.XMLHTTP");
 		}
-
+		var buf;
+		var _ip;
 		xhr.open("GET","https://httpbin.org/get",true);
-		//xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader('Content-Type', 'application/json');
 		xhr.addEventListener('load',function() {
-			var buf = this.responseText;
 			if(xhr.status ===200){
-				document.write("We got data! <br/><br/>" + buf);
+				buf = this.responseText;
+				_ip = JSON.parse(buf).origin;
+
+				document.write("<div style=\"text-align: center;\">We got data! <br/><br/></div>" + buf+"<div style=\"text-align: center;\"><br/><br/>final<br/><br/></div>");
+				document.write("<div style=\"text-align: center;font: bold italic 2.0em/1.0em consolas;\"> Your IP is "+_ip+"<br/><br/></div>")
 			}
 			else{
 				document.write("Error loading page\n");
 			}
-		},false)
 
-		//xhr.overrideMimeType('text/json');
-		xhr.send();	
-	})	
+		},false);
+
+		xhr.overrideMimeType('text/json');
+		xhr.send();
+	});	
 }
