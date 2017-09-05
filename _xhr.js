7f9c0f11a38bcb5d2d830d401bb7b89c411c7b57
Спасибо <ty662@naver.com>
2017-09-05 11:23:30 +0900

    Update _xhr.js

--- _xhr.js
+++ _xhr.js
@@ -2,19 +2,25 @@ window.onload = function(){
 	var bt1 = document.getElementById('_bt1');
 
 	bt1.addEventListener('click', function() {
-		var xhr = new XMLHttpRequest();
+		var xhr
+		if(window.XMLHttpRequest){
+			xhr = new XMLHttpRequest();
+		} else if (window.ActiveXObject){
+			xhr = new ActiveXObject("Microsoft.XMLHTTP");
+		}
 
 		xhr.open("GET","https://slack.com/api/api.test",true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
 		xhr.addEventListener('load',function() {
 			if(xhr.status ===200){
-				alert("We got data: " + xhr.respose);
+				document.write("We got data! <br/><br/>" + xhr.responseText);
 			}
 			else{
-				dump("Error loading page\n");
+				document.write("Error loading page\n");
 			}
 		},false)
 
-		//xhr.overrideMimeType('text/json');
+		xhr.overrideMimeType('text/json');
 		xhr.send();	
 	})	
 }
